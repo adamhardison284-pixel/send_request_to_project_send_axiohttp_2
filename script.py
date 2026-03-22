00a65bd9f745975d7efd9d0842ffe777f11e0250
adamhardison284-pixel <adamhardison284@gmail.com>
2026-03-23 00:12:34 +0100

    Update script.py

--- script.py
+++ script.py
@@ -6,9 +6,9 @@ import random
 
 API_URLS = [
     "https://lezrsmwspvrdahngtvli.supabase.co/functions/v1/PojectSend",
-    #"https://vuudkapcuwtkepeqkpfx.supabase.co/functions/v1/PojectSend",
+    "https://vuudkapcuwtkepeqkpfx.supabase.co/functions/v1/PojectSend",
     "https://guypgtlqmsoyjvsspfro.supabase.co/functions/v1/PojectSend",
-    #"https://neighiwqhabqzhakwupe.supabase.co/functions/v1/PojectSend",
+    "https://neighiwqhabqzhakwupe.supabase.co/functions/v1/PojectSend",
     "https://gmweskylxvbhhgrcftbo.supabase.co/functions/v1/PojectSend",
 ]
 
